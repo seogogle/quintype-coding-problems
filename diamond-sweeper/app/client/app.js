0e2ca309c004167ee58bee70334ae32db90c1a57
CIBER-INDIA\kcherukuri <seogogle@gmail.com>
2018-06-12 13:48:25 +0530

    First part completed
    The game board has 8x8 squares (initially, all represented by question marks)
    There are 8 diamonds hidden on the board, each diamond behind one of the squares
    When the user clicks on a square
    If the square was hiding a diamond, the diamond appears
    Otherwise, the square is opened, and blank
    The game ends when all diamonds are found. The user's score is the number of squares still left unturned.

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,5 +1,37 @@
+  var hiddenDiamonds = [];//array for hidden diamond location
+  var allCells;
+  var i,u;
 global.startApp = function(container) {
   console.log("Here is the container:", container);
-  var hiddenDiamonds = [];
-  
+  allCells = $('.cell');
+  allCells.attr('class', 'cell unknown');
+
+  //putting random diamonds in 64 cells
+  for(i=0; i<8; i++){
+	hiddenDiamonds.push(parseInt(Math.random(0,.6)*64));
+  }
+
+  //putting hidden diamonds in boxes
+  for(u=0; u<hiddenDiamonds.length && hiddenDiamonds.length>0;u++){
+  	allCells[hiddenDiamonds[u]].setAttribute('class', 'cell diamond unknown');
+  }
+
+  //on click evvent for diamonds when clicked on hidden diamond
+  $('.cell.diamond.unknown').click(function(){
+		$(this).removeClass('unknown');
+		score();
+  })
+
+  //on click event for empty diamond cells
+  $('.cell.unknown').click(function(){
+		$(this).removeClass('unknown');
+		score();
+  })
+
+  function score(){
+  	/*if($('.cell.diamond.unknown').length()==0)
+  		alert('Game Over!  Your Score'+$('.cell.unknown').length)
+  	else*/
+  		$('.Score').text($('.cell.unknown').length);
+  }
 }
